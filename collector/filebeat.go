26009924b35d99a714847a41063095313f0312e6
Sergiy Chayka <schayka@gmail.com>
2019-07-28 22:37:22 -0400

    corrupted log removal procedure

--- collector/filebeat.go
+++ collector/filebeat.go
@@ -3,6 +3,7 @@ package collector
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"os/exec"
 	"regexp"
 	"strings"
@@ -88,13 +89,25 @@ func getHarvesterErrors(stats *Stats) float64 {
 			errStr = errStr[1:]
 		}
 
-		for _, v := range errStr {
-			checkResult := isHarvesterError(r, v)
+		for _, errLine := range errStr {
+			checkResult := isHarvesterError(r, errLine)
 
 			if len(checkResult) > 0 {
 				lastError = checkResult
 
 				errorCount = errorCount + 1
+
+				errSplit := strings.Split(errLine, " ")
+
+				logName := errSplit[len(errSplit)-1]
+
+				err = os.Remove(logName)
+
+				if err != nil {
+					fmt.Printf("DeleteLog ERROR %v, log [%s]", err, logName)
+				} else {
+					fmt.Printf("DeleteLog DELETED log [%s]", logName)
+				}
 			}
 		}
 	}
